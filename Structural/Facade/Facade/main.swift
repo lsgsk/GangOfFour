6428a2cc232cadcf822ece83186c772d89f08618
Alexsandr Lysov <lsgsk@rambler.ru>
2021-07-31 12:18:13 +0300

    Added Facade

---
+++ Structural/Facade/Facade/main.swift
@@ -0,0 +1,10 @@
+//
+//  Created by Лысов Александр on 31.07.2021.
+//
+
+import Foundation
+
+let input = Stream()
+let output = Stream()
+let compiler = Compiler()
+compiler.compile(input: input, output: output)
